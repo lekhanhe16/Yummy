da86cff2d2746be27ab8de2bbe75f46b0d5eb734
lekhanhe16 <55609697+lekhanhe16@users.noreply.github.com>
2023-03-18 11:16:46 +0700

    Add launch screen and Onboarding

--- Yummy/Modules/Onboarding/OnboardingViewController.swift
+++ Yummy/Modules/Onboarding/OnboardingViewController.swift
@@ -34,15 +34,15 @@ extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
     }
 }
 
-extension OnboardingViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-    
-    
-}
+//extension OnboardingViewController: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        <#code#>
+//    }
+//    
+//    
+//}
 
